5659b2946cd8e2a3ba2163d178cffb0fe766d41b
Max Knutsen <mknutsen99@gmail.com>
2022-02-11 20:01:29 -0800

    formatting with black

--- stockcv/flask_entry.py
+++ stockcv/flask_entry.py
@@ -1,4 +1,4 @@
-from flask import Flask, request,send_from_directory
+from flask import Flask, request, send_from_directory
 from pystache import Renderer
 from os import makedirs
 from shutil import rmtree
@@ -6,7 +6,8 @@ from pathlib import Path
 
 
 class FlaskException(Exception):
-  """This is what happens when flask fails"""
+    """This is what happens when flask fails"""
+
 
 file_path = Path(__file__) / ".."
 static_web_folder_name = "static_web"
@@ -18,6 +19,11 @@ dashboard_file_path = static_web_folder_path / (dashboard_file_name + ".html")
 dashboard_file_path_str = dashboard_file_path.resolve()
 
 
+_RATE_KEYWORD = "RATE"
+_LENGTH_KEYWORD = "length"
+_RANGE_KEYWORD = "range"
+_BASE_KEYWORD = "base"
+_KEYWORD_LIST = [_RATE_KEYWORD, _LENGTH_KEYWORD, _RANGE_KEYWORD, _BASE_KEYWORD]
 
 rmtree(static_web_folder_path_str)
 makedirs(static_web_folder_path_str)
@@ -52,38 +58,38 @@ Hello world dashboard
 </script>
 """
 
-element_names = ["rate", "spread", "length", "steps"]
-input_dict = {
-  "names" : [{"name" : element}for element in element_names]
-}
+input_dict = {"names": [{"name": element} for element in _KEYWORD_LIST]}
 renderer = Renderer()
 
 with open(dashboard_file_path_str, "w") as file:
-  file.write(renderer.render(input_template, input_dict))
+    file.write(renderer.render(input_template, input_dict))
 
 GLOBAL_DATA_CALLBACK = None
- 
-app = Flask(__name__, static_url_path='', static_folder=static_web_folder_path_str)
-def main(callback=None):
-  global GLOBAL_DATA_CALLBACK
-  if callback:
-    GLOBAL_DATA_CALLBACK = callback
-  app.run(host='localhost', port=5000)
+app = Flask(__name__, static_url_path="", static_folder=static_web_folder_path_str)
 
-@app.route('/result', methods=['GET', 'POST'])
+
+def main(callback=None):
+    global GLOBAL_DATA_CALLBACK
+    if callback:
+        GLOBAL_DATA_CALLBACK = callback
+    app.run(host="localhost", port=5000)
+
+
+@app.route("/result", methods=["GET", "POST"])
 def parse_request():
     global GLOBAL_DATA_CALLBACK
     data = request.data  # data is empty
-    data_source_name = request.form.get("name","")
-    data_value = request.form.get("value","")
+    data_source_name = request.form.get("name", "")
+    data_value = request.form.get("value", "")
     print("in here abc123", data, type(data), request.form)
     if GLOBAL_DATA_CALLBACK:
-      if not (data_source_name and data_value):
-        raise FlaskException()
-      GLOBAL_DATA_CALLBACK(data_source_name, data_value)
+        if not (data_source_name and data_value):
+            raise FlaskException()
+        GLOBAL_DATA_CALLBACK(data_source_name, data_value)
     # print("data dict: ", data.__dict__)
-    return 'ak'
+    return "ak"
     # need posted data here
 
+
 if __name__ == "__main__":
-	main()  
+    main()
--- stockcv/main.py
+++ stockcv/main.py
@@ -5,7 +5,15 @@ from pandas import DataFrame
 from threading import Thread, Lock
 from math import floor
 
-from flask_entry import main as flask_entry_point
+from flask_entry import (
+    main as flask_entry_point,
+    _KEYWORD_LIST,
+    _RANGE_KEYWORD,
+    _LENGTH_KEYWORD,
+    _BASE_KEYWORD,
+    _RATE_KEYWORD,
+)
+
 ticker: str = "PTON"
 start_date: str = "2020-01-01"
 end_date: str = "2022-01-01"
@@ -21,155 +29,169 @@ output_port_name: str = "mio"
 input_port_name: str = "Arturia BeatStep Pro Arturia BeatStepPro"
 stock_data = None
 
+
 def math():
-	global stock_data
-	df: DataFrame = stock_price_download(ticker, start_date, end_date)
-	df = df['Open']
+    global stock_data
+    df: DataFrame = stock_price_download(ticker, start_date, end_date)
+    df = df["Open"]
 
-	num_entries: int = len(df)
-	step_factor: int = floor(num_entries / steps)
-	# NOTE: if the goal is to get 32 final steps, the floor causes the actual number you get to be slightly higher (ex.: ~34)
+    num_entries: int = len(df)
+    step_factor: int = floor(num_entries / steps)
+    # NOTE: if the goal is to get 32 final steps, the floor causes the actual number you get to be slightly higher (ex.: ~34)
 
-	filtered_df = df.iloc[::step_factor]
+    filtered_df = df.iloc[::step_factor]
 
+    min_value: float = df.min()
+    max_value: float = df.max()
+    value_range: float = max_value - min_value
 
-	min_value: float = df.min()
-	max_value: float = df.max()
-	value_range: float = max_value - min_value
+    normalized_df = (filtered_df - min_value) / value_range
 
-	normalized_df = (filtered_df - min_value) / value_range
+    # mixed_data: List[float] = [
+    #     ((normalized - raw) * mix) + raw
+    #     for raw, normalized in zip(step_values, normalized_data)
+    # ]
 
-	# mixed_data: List[float] = [
-	#     ((normalized - raw) * mix) + raw
-	#     for raw, normalized in zip(step_values, normalized_data)
-	# ]
-
-	return normalized_df.tolist()
+    return normalized_df.tolist()
 
 
 class Clock:
-	def __init__(self, callback_fn) -> None:
-		self.callback_fn: Callable[[Optional[Message]], None] = callback_fn
-		pass
+    def __init__(self, callback_fn) -> None:
+        self.callback_fn: Callable[[Optional[Message]], None] = callback_fn
+        pass
 
-	def tic(self, message: Optional[Message] = None) -> None:
-		self.callback_fn(message)
+    def tic(self, message: Optional[Message] = None) -> None:
+        self.callback_fn(message)
 
-	def message(self, message: Message) -> None:
-		self.callback_fn(message)
+    def message(self, message: Message) -> None:
+        self.callback_fn(message)
 
 
 def input_loop(port_name: str, clock: Clock) -> None:
-	print("input loop")
-	try:
-		port = open_input(port_name)
-	except:
-		print("input failed")
-		print(get_input_names())
-		raise
-	print("up and running")
-	for message in port:
-		if message.type == "clock":
-			clock.tic()
-		else:
-			# thru port hack
-			# print("input loop sending", message)
-			clock.tic(message)
+    print("input loop")
+    try:
+        port = open_input(port_name)
+    except:
+        print("input failed")
+        print(get_input_names())
+        raise
+    print("up and running")
+    for message in port:
+        if message.type == "clock":
+            clock.tic()
+        else:
+            # thru port hack
+            # print("input loop sending", message)
+            clock.tic(message)
 
 
 class CvSequence:
-	def __init__(self, port, sequence, tics_per_step, channel, cc) -> None:
-		self.lock = Lock()
-		self.step_index = 0
-		self.tic_index = 0
-		self.port = port
-		self.sequence = sequence
-		self.sequence_length = len(sequence)
-		self.channel = channel
-		self.cc = cc
-		self.tics_per_step = tics_per_step
-	
-	def alter(self, name, value):
-		pass
+    def __init__(self, port, sequence, tics_per_step, channel, cc) -> None:
+        self.lock = Lock()
+        self.step_index = 0
+        self.tic_index = 0
+        self.port = port
+        self.sequence = sequence
+        self.sequence_length = len(sequence)
+        self.channel = channel
+        self.cc = cc
+        self.tics_per_step = tics_per_step
 
-	def _increment_step_index(self):
-		# print("next_step", self.step_index)
-		self.step_index += 1
-		if self.step_index >= self.sequence_length:
-			print("----LOOP-------")
-			self.step_index = 0
+    def alter(self, name, value):
+      if name == _RATE_KEYWORD:
+        pass
+      elif name == _LENGTH_KEYWORD:
+        pass
+      elif name == _RANGE_KEYWORD:
+        pass
+      elif name == _BASE_KEYWORD:
+        pass
 
-	def tick(self):
-		with self.lock:
-			self.tic_index += 1
-			if self.tic_index >= self.tics_per_step:
-				self.tic_index = 0
-				self._step()
+    def _increment_step_index(self):
+        # print("next_step", self.step_index)
+        self.step_index += 1
+        if self.step_index >= self.sequence_length:
+            print("----LOOP-------")
+            self.step_index = 0
 
-	def _step(self):
-		# print("step")
-		normalized_value: int = floor(127 * self.sequence[self.step_index])
-		message = Message(
-			type="control_change",
-			channel=self.channel,
-			control=self.cc,
-			value=normalized_value,
-		)
-		self.port.send(message)
-		print(message)
-		self._increment_step_index()
+    def tick(self):
+        with self.lock:
+            self.tic_index += 1
+            if self.tic_index >= self.tics_per_step:
+                self.tic_index = 0
+                self._step()
+
+    def _step(self):
+        # print("step")
+        normalized_value: int = floor(127 * self.sequence[self.step_index])
+        message = Message(
+            type="control_change",
+            channel=self.channel,
+            control=self.cc,
+            value=normalized_value,
+        )
+        self.port.send(message)
+        print(message)
+        self._increment_step_index()
+
 
 def data_callback(name, value):
-	global sequence
-	print(f"abc123 data callback {name} {value}")
-	sequence.alter(name, value)
+    global sequence
+    print(f"abc123 data callback {name} {value}")
+    sequence.alter(name, value)
+
 
 def main():
-	global sequence
-	flask_entry_point(callback=data_callback)
-	try:
-		port_out = open_output(output_port_name)
-	except:
-		print(get_output_names())
-		raise
+    global sequence
+    flask_entry_point(callback=data_callback)
+    try:
+        port_out = open_output(output_port_name)
+    except:
+        print(get_output_names())
+        raise
 
-	normalized_sequence_data = math()
-	sequence = CvSequence(port= port_out, sequence=normalized_sequence_data, tics_per_step = tics_per_step, channel = channel, cc = cc)
+    normalized_sequence_data = math()
+    sequence = CvSequence(
+        port=port_out,
+        sequence=normalized_sequence_data,
+        tics_per_step=tics_per_step,
+        channel=channel,
+        cc=cc,
+    )
 
-	def clock_tic_callback(message: Optional[Message] = None) -> None:
-		if not message:
-			sequence.tick()
-		else:
-			# print("clock tick callback", message)
-			port_out.send(message)
+    def clock_tic_callback(message: Optional[Message] = None) -> None:
+        if not message:
+            sequence.tick()
+        else:
+            # print("clock tick callback", message)
+            port_out.send(message)
 
+    clock = Clock(clock_tic_callback)
+    input_thread = Thread(target=input_loop, args=(input_port_name, clock))
+    input_thread.start()
 
-	clock = Clock(clock_tic_callback)
-	input_thread = Thread(target=input_loop, args=(input_port_name, clock))
-	input_thread.start()
 
 if __name__ == "__main__":
-	main()
+    main()
 
-	from PyQt5 import QtWidgets
-	from pyqtgraph import PlotWidget, plot
-	import pyqtgraph as pg
-	import sys  # We need sys so that we can pass argv to QApplication
-	import os
+    from PyQt5 import QtWidgets
+    from pyqtgraph import PlotWidget, plot
+    import pyqtgraph as pg
+    import sys  # We need sys so that we can pass argv to QApplication
+    import os
 
-	class MainWindow(QtWidgets.QMainWindow):
+    class MainWindow(QtWidgets.QMainWindow):
+        def __init__(self, *args, **kwargs):
+            super(MainWindow, self).__init__(*args, **kwargs)
 
-		def __init__(self, *args, **kwargs):
-			super(MainWindow, self).__init__(*args, **kwargs)
+            self.graphWidget = pg.PlotWidget()
+            self.setCentralWidget(self.graphWidget)
+            x = [i for i in range(len(stock_data))]
+            y = stock_data
+            # plot data: x, y values
+            self.graphWidget.plot(x, y)
 
-			self.graphWidget = pg.PlotWidget()
-			self.setCentralWidget(self.graphWidget)
-			x = [i for i in range(len(stock_data))]
-			y = stock_data
-			# plot data: x, y values
-			self.graphWidget.plot(x, y)
-
-	app = QtWidgets.QApplication(sys.argv)
-	mw = MainWindow()
-	mw.show()
-	sys.exit(app.exec_())
+    app = QtWidgets.QApplication(sys.argv)
+    mw = MainWindow()
+    mw.show()
+    sys.exit(app.exec_())
